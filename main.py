f80162afa54ba78eddcd14f15bb20af22b1fa8c5
Vinod <118246607+vinodvk00@users.noreply.github.com>
2024-01-25 13:29:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -117,5 +117,5 @@ def blog():
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
-    # app.run(debug = False, host = '0.0.0.0')
+    # app.run(debug=True)
+    app.run(debug = False, host = '0.0.0.0')
